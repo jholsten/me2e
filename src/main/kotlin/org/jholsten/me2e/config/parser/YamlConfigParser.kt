b3532c78243fb40269975adf78b3049b630d6d45
Joana <jholsten@uni-bremen.de>
2023-07-08 13:30:36 +0200

    adds method to yaml config parser

--- src/main/kotlin/org/jholsten/me2e/config/parser/YamlConfigParser.kt
+++ src/main/kotlin/org/jholsten/me2e/config/parser/YamlConfigParser.kt
@@ -1,8 +1,13 @@
 package org.jholsten.me2e.config.parser
 
+import org.jholsten.me2e.config.model.TestConfig
+
 /**
  * Class for parsing test configuration defined in YAML file.
  */
 class YamlConfigParser: ConfigParser {
-
+    override fun parseFile(filename: String): TestConfig {
+        
+        TODO("Not yet implemented")
+    }
 }
