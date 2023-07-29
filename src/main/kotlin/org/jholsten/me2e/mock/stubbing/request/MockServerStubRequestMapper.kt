3aa31372b39e246cc18d5a69be1c3eb873c6cefe
Joana <jholsten@uni-bremen.de>
2023-07-29 23:07:34 +0200

    uses weighted matching to show nearly missed stubs

--- src/main/kotlin/org/jholsten/me2e/mock/stubbing/request/MockServerStubRequestMapper.kt
+++ src/main/kotlin/org/jholsten/me2e/mock/stubbing/request/MockServerStubRequestMapper.kt
@@ -5,22 +5,29 @@ import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.http.Request
 import com.github.tomakehurst.wiremock.matching.MatchResult
 import com.github.tomakehurst.wiremock.matching.ValueMatcher
+import com.github.tomakehurst.wiremock.matching.WeightedMatchResult.weight
 
 internal class MockServerStubRequestMapper private constructor() {
     companion object {
         /**
-         * Maps stub request to equivalent stub for WireMock.
+         * Maps stub request to equivalent stub for WireMock. Uses weighted matching to show nearly missed stubs.
+         * (as in [com.github.tomakehurst.wiremock.matching.RequestPattern])
          */
         @JvmStatic
         fun toWireMockStubRequestMatcher(stubRequest: MockServerStubRequest): MappingBuilder {
             val matcher = object : ValueMatcher<Request> {
                 override fun match(request: Request): MatchResult {
-                    // TODO: Introduce partial matches (see [com.github.tomakehurst.wiremock.matching.RequestPattern])
-                    return if (stubRequest.matches(request)) {
-                        MatchResult.exactMatch()
-                    } else {
-                        MatchResult.noMatch()
-                    }
+                    val results = listOf(
+                        stubRequest.pathMatches(request.url) to 10.0,
+                        stubRequest.methodMatches(request.method) to 10.0,
+                        stubRequest.headersMatch(request.headers) to 1.0,
+                        stubRequest.queryParametersMatch(request) to 1.0,
+                        stubRequest.bodyPatternsMatch(request) to 1.0,
+                    )
+
+                    return MatchResult.aggregateWeighted(results.map {
+                        weight(if (it.first) MatchResult.exactMatch() else MatchResult.noMatch(), it.second)
+                    })
                 }
             }
             return WireMock.requestMatching(matcher)
