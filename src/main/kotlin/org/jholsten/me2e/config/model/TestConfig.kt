6670b796ef7d01b975965ea4a84bccfaed3ebfe2
Joana <jholsten@uni-bremen.de>
2023-07-29 13:16:44 +0200

    moves port in test config to mock server definition

--- src/main/kotlin/org/jholsten/me2e/config/model/TestConfig.kt
+++ src/main/kotlin/org/jholsten/me2e/config/model/TestConfig.kt
@@ -3,7 +3,9 @@ package org.jholsten.me2e.config.model
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import org.jholsten.me2e.config.utils.ContainerMapDeserializer
+import org.jholsten.me2e.config.utils.MockServerMapDeserializer
 import org.jholsten.me2e.container.Container
+import org.jholsten.me2e.mock.MockServer
 
 /**
  * Model containing the test configuration to use.
@@ -19,7 +21,8 @@ class TestConfig(
      * HTTP web servers mocking third party services.
      */
     @JsonProperty("mock-servers")
-    val mockServers: MockServerConfig? = null,
+    @JsonDeserialize(using = MockServerMapDeserializer::class)
+    val services: Map<String, MockServer> = mapOf(),
 ) {
 
 }
--- src/main/kotlin/org/jholsten/me2e/mock/MockServer.kt
+++ src/main/kotlin/org/jholsten/me2e/mock/MockServer.kt
@@ -1,5 +1,8 @@
 package org.jholsten.me2e.mock
 
+import com.github.tomakehurst.wiremock.WireMockServer
+import org.jholsten.me2e.mock.stubbing.MockServerStub
+
 /**
  * Model representing a third party service to be mocked.
  */
@@ -10,8 +13,20 @@ class MockServer(
     val name: String,
 
     /**
+     * Port where this mocked web server is exposed
+     */
+    val port: Int,
+
+    /**
      * List of paths to stub definitions. The files need to be located in `resources` folder.
      */
-    val stubs: List<String> = listOf(),
+    stubs: List<String> = listOf(),
 ) {
+    // TODO: This is the equivalent to the wire mock server instance (i.e. needs reset, stubFor, getEvents etc.)
+    val stubs: List<MockServerStub>
+
+    init {
+        this.stubs = listOf() // stubs TODO: Read from file + call stubFor
+        val wireMockServer = WireMockServer()
+    }
 }
