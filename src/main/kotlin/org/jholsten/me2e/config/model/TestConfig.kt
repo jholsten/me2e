94114ed382bc67111630b7943ca323e0250dd49c
Joana <jholsten@uni-bremen.de>
2023-12-23 11:37:17 +0100

    removes unneccessary annotation

--- src/main/kotlin/org/jholsten/me2e/config/model/TestConfig.kt
+++ src/main/kotlin/org/jholsten/me2e/config/model/TestConfig.kt
@@ -1,8 +1,5 @@
 package org.jholsten.me2e.config.model
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import org.jholsten.me2e.config.utils.TestEnvironmentConfigDeserializer
-
 /**
  * Model containing the test configuration to use.
  */
@@ -10,6 +7,5 @@ class TestConfig(
     /**
      * Configuration of the test environment.
      */
-    @JsonDeserialize(using = TestEnvironmentConfigDeserializer::class)
     val environment: TestEnvironmentConfig,
 )
