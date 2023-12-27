d23fa827754ad93e11807e931420db4abe5fa4cd
Joana <jholsten@uni-bremen.de>
2023-12-27 17:40:57 +0100

    adds integration tests for okhttp client

---
+++ src/test/kotlin/org/jholsten/me2e/request/client/OkHttpClientIT.kt
@@ -0,0 +1,161 @@
+package org.jholsten.me2e.request.client
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import org.awaitility.Awaitility
+import org.awaitility.Durations
+import org.jholsten.me2e.request.interceptor.RequestInterceptor
+import org.jholsten.me2e.request.model.*
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+import kotlin.test.*
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class OkHttpClientIT {
+
+    private val wireMockServer = WireMockServer()
+
+    @BeforeAll
+    fun beforeAll() {
+        wireMockServer.start()
+        Awaitility.await().atMost(Durations.FIVE_SECONDS).until { wireMockServer.isRunning }
+    }
+
+    @AfterAll
+    fun afterAll() {
+        wireMockServer.stop()
+    }
+
+    @BeforeTest
+    fun beforeTest() {
+        wireMockServer.stubFor(
+            WireMock.any(WireMock.anyUrl())
+                .willReturn(
+                    WireMock.aResponse()
+                        .withStatus(200)
+                        .withBody("Some Response")
+                        .withHeader("Content-Type", "text/plain")
+                )
+        )
+    }
+
+    @AfterTest
+    fun afterTest() {
+        wireMockServer.resetAll()
+    }
+
+    @ParameterizedTest
+    @EnumSource(HttpMethod::class, names = ["GET", "DELETE", "OPTIONS"])
+    fun `Executing request without body should succeed`(httpMethod: HttpMethod) {
+        val httpClient = OkHttpClient.Builder()
+            .withBaseUrl(wireMockServer.baseUrl())
+            .build()
+
+        val headers = mapOf("Name" to listOf("Value"))
+        val request = HttpRequest.Builder()
+            .withUrl(wireMockServer.baseUrl(), "/search", queryParams = mapOf("id" to "123"))
+            .withMethod(httpMethod)
+            .withHeaders(headers)
+            .build()
+
+        val response = httpClient.execute(request)
+
+        assertResponseAsExpected(response)
+        assertRequestWasSent(
+            url = wireMockServer.url("/search?id=123"),
+            method = httpMethod,
+            headers = headers,
+            body = null,
+        )
+    }
+
+    @ParameterizedTest
+    @EnumSource(HttpMethod::class, names = ["PUT", "POST", "PATCH", "DELETE"])
+    fun `Executing request with body should succeed`(httpMethod: HttpMethod) {
+        val httpClient = OkHttpClient.Builder()
+            .withBaseUrl(wireMockServer.baseUrl())
+            .build()
+
+
+        val headers = mapOf("Name" to listOf("Value"))
+        val body = HttpRequestBody("Some Request", MediaType.TEXT_PLAIN_UTF8)
+        val request = HttpRequest.Builder()
+            .withUrl(wireMockServer.baseUrl(), "/search", queryParams = mapOf("id" to "123"))
+            .withMethod(httpMethod)
+            .withHeaders(headers)
+            .withBody(body)
+            .build()
+
+        val response = httpClient.execute(request)
+
+        assertResponseAsExpected(response)
+        assertRequestWasSent(
+            url = wireMockServer.url("/search?id=123"),
+            method = httpMethod,
+            headers = headers,
+            body = body,
+        )
+    }
+
+    @Test
+    fun `Executing request with interceptor should call interceptor`() {
+        val interceptor = object : RequestInterceptor {
+            override fun intercept(chain: RequestInterceptor.Chain): HttpResponse {
+                val request = chain.getRequest().newBuilder()
+                    .addHeader("Authorization", "Bearer 123")
+                    .build()
+                return chain.proceed(request)
+            }
+        }
+
+        val httpClient = OkHttpClient.Builder()
+            .withBaseUrl(wireMockServer.baseUrl())
+            .withRequestInterceptors(listOf(interceptor))
+            .build()
+
+
+        val headers = mapOf("Name" to listOf("Value"))
+        val request = HttpRequest.Builder()
+            .withUrl(wireMockServer.baseUrl(), "/search", queryParams = mapOf("id" to "123"))
+            .withMethod(HttpMethod.GET)
+            .withHeaders(headers)
+            .build()
+
+        val response = httpClient.execute(request)
+
+        assertResponseAsExpected(response)
+        assertRequestWasSent(
+            url = wireMockServer.url("/search?id=123"),
+            method = HttpMethod.GET,
+            headers = headers.plus("Authorization" to listOf("Bearer 123")),
+            body = null,
+        )
+    }
+
+    private fun assertResponseAsExpected(response: HttpResponse) {
+        assertEquals(200, response.code)
+        assertNotNull(response.body)
+        assertEquals("Some Response", response.body!!.asString())
+        assertTrue(response.headers.containsKey("content-type"))
+        assertEquals(listOf("text/plain"), response.headers["content-type"])
+    }
+
+    private fun assertRequestWasSent(url: String, method: HttpMethod, headers: Map<String, List<String>>, body: HttpRequestBody?) {
+        assertEquals(1, wireMockServer.allServeEvents.size)
+        val request = wireMockServer.allServeEvents.first().request
+        assertEquals(url, request.absoluteUrl)
+        assertEquals(method, HttpMethod.valueOf(request.method.value()))
+        for ((header, values) in headers) {
+            assertEquals(values, request.headers.getHeader(header).values())
+        }
+        if (body == null) {
+            assertTrue(request.body == null || request.body.isEmpty())
+        } else {
+            assertTrue(request.body != null && request.body.isNotEmpty())
+            assertEquals(body.asString(), request.bodyAsString)
+        }
+    }
+}
