acc40b493de5031c60e20bed25a01729abdccacc
Joana <jholsten@uni-bremen.de>
2024-01-02 18:57:22 +0100

    changes message of parse exception to be nullable

--- src/main/kotlin/org/jholsten/me2e/parsing/exception/ParseException.kt
+++ src/main/kotlin/org/jholsten/me2e/parsing/exception/ParseException.kt
@@ -4,5 +4,5 @@ package org.jholsten.me2e.parsing.exception
  * Exception that indicates an error occurring on parsing.
  */
 open class ParseException(
-    message: String,
+    message: String?,
 ) : RuntimeException(message)
