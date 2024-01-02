9b1b5b52ef5fd0889ce0ed50dd1b2b4f118b69cd
Joana <jholsten@uni-bremen.de>
2024-01-02 19:39:57 +0100

    deletes http response builder

--- src/main/kotlin/org/jholsten/me2e/request/model/HttpResponse.kt
+++ src/main/kotlin/org/jholsten/me2e/request/model/HttpResponse.kt
@@ -1,7 +1,5 @@
 package org.jholsten.me2e.request.model
 
-import okhttp3.internal.toImmutableMap
-
 /**
  * Model representing an HTTP response.
  */
@@ -37,74 +35,4 @@ class HttpResponse internal constructor(
      * Response body of this response.
      */
     val body: HttpResponseBody?,
-) {
-
-    /**
-     * Returns new builder instance initialized with the properties of this HTTP response.
-     * This allows to create new instances with partly modified properties.
-     */
-    fun newBuilder(): Builder {
-        return Builder(this)
-    }
-
-    class Builder constructor() {
-        private var request: HttpRequest? = null
-        private var protocol: String? = null
-        private var message: String? = null
-        private var code: Int? = null
-        private var headersBuilder: HttpHeaders.Builder = HttpHeaders.Builder()
-        private var body: HttpResponseBody? = null
-
-        internal constructor(httpResponse: HttpResponse) : this() {
-            this.request = httpResponse.request
-            this.protocol = httpResponse.protocol
-            this.message = httpResponse.message
-            this.code = httpResponse.code
-            this.headersBuilder = httpResponse.headers.newBuilder()
-            this.body = httpResponse.body
-        }
-
-        fun withRequest(request: HttpRequest) = apply {
-            this.request = request
-        }
-
-        fun withProtocol(protocol: String) = apply {
-            this.protocol = protocol
-        }
-
-        fun withMessage(message: String) = apply {
-            this.message = message
-        }
-
-        fun withCode(code: Int) = apply {
-            this.code = code
-        }
-
-        fun withHeaders(headers: HttpHeaders) = apply {
-            this.headersBuilder = headers.newBuilder()
-        }
-
-        fun addHeader(key: String, value: String) = apply {
-            headersBuilder.add(key, value)
-        }
-
-        fun withBody(body: HttpResponseBody?) = apply {
-            this.body = body
-        }
-
-        fun build(): HttpResponse {
-            val request = this.request ?: throw IllegalArgumentException("Request cannot be null")
-            val protocol = this.protocol ?: throw IllegalArgumentException("Protocol cannot be null")
-            val message = this.message ?: throw IllegalArgumentException("Message cannot be null")
-            val code = this.code ?: throw IllegalArgumentException("Code cannot be null")
-            return HttpResponse(
-                request = request,
-                protocol = protocol,
-                message = message,
-                code = code,
-                headers = headersBuilder.build(),
-                body = body,
-            )
-        }
-    }
-}
+)
