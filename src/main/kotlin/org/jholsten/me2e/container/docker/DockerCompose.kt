e3f1b1ee2a4455f50b5fe77cd9ec636ae4354b57
Joana <jholsten@uni-bremen.de>
2024-01-03 20:41:05 +0100

    adds wrapper for docker compose

---
+++ src/main/kotlin/org/jholsten/me2e/container/docker/DockerCompose.kt
@@ -0,0 +1,141 @@
+package org.jholsten.me2e.container.docker
+
+import org.testcontainers.containers.ContainerState
+import org.testcontainers.containers.wait.strategy.WaitStrategy
+import org.testcontainers.containers.DockerComposeContainer as DockerComposeV1
+import org.testcontainers.containers.ComposeContainer as DockerComposeV2
+import java.io.File
+import java.util.Optional
+
+/**
+ * Custom wrapper for testcontainers Docker-Compose classes [DockerComposeV1] and [DockerComposeV2].
+ * Since testcontainers uses two different classes for version 1 and 2, which do not have a common supertype, it is otherwise difficult
+ * to handle instances of both classes. Therefore, this class provides a common interface for both classes.
+ */
+internal class DockerCompose(
+    identifier: String,
+    file: File,
+    private val version: DockerComposeVersion
+) {
+    /**
+     * Kotlin wrapper for [DockerComposeV1] to avoid type issue.
+     * See [GitHub Issue #1010](https://github.com/testcontainers/testcontainers-java/issues/1010)
+     */
+    internal class KDockerComposeV1(identifier: String, file: File) : DockerComposeV1<KDockerComposeV1>(identifier, file)
+
+    /**
+     * Lazy reference to testcontainers implementation for Docker-Compose version 1.
+     */
+    private val v1: KDockerComposeV1 by lazy {
+        KDockerComposeV1(identifier, file)
+    }
+
+    /**
+     * Lazy reference to testcontainers implementation for Docker-Compose version 2.
+     */
+    private val v2: DockerComposeV2 by lazy {
+        DockerComposeV2(identifier, file)
+    }
+
+    /**
+     * Whether to use a local Docker-Compose binary instead of a container.
+     * @see DockerComposeV1.withLocalCompose
+     * @see DockerComposeV2.withLocalCompose
+     */
+    fun withLocalCompose(localCompose: Boolean) = apply {
+        when (this.version) {
+            DockerComposeVersion.V1 -> v1.withLocalCompose(localCompose)
+            DockerComposeVersion.V2 -> v2.withLocalCompose(localCompose)
+        }
+    }
+
+    /**
+     * Whether to always build images before starting containers.
+     * @see DockerComposeV1.withBuild
+     * @see DockerComposeV2.withBuild
+     */
+    fun withBuild(build: Boolean) = apply {
+        when (this.version) {
+            DockerComposeVersion.V1 -> v1.withBuild(build)
+            DockerComposeVersion.V2 -> v2.withBuild(build)
+        }
+    }
+
+    /**
+     * Whether to remove images after containers shut down.
+     * @see DockerComposeV1.withRemoveImages
+     * @see DockerComposeV2.withRemoveImages
+     */
+    fun withRemoveImages(removeImages: DockerComposeRemoveImagesStrategy) = apply {
+        when (this.version) {
+            DockerComposeVersion.V1 -> v1.withRemoveImages(removeImages.toV1())
+            DockerComposeVersion.V2 -> v2.withRemoveImages(removeImages.toV2())
+        }
+    }
+
+    /**
+     * Whether to remove volumes after containers shut down.
+     * @see DockerComposeV1.withRemoveVolumes
+     * @see DockerComposeV2.withRemoveVolumes
+     */
+    fun withRemoveVolumes(removeVolumes: Boolean) = apply {
+        when (this.version) {
+            DockerComposeVersion.V1 -> v1.withRemoveVolumes(removeVolumes)
+            DockerComposeVersion.V2 -> v2.withRemoveVolumes(removeVolumes)
+        }
+    }
+
+    /**
+     * Specify the [WaitStrategy] to use to determine if the container is ready.
+     * @param serviceName the name of the service to wait for
+     * @param waitStrategy the wait strategy to use
+     * @see DockerComposeV1.waitingFor
+     * @see DockerComposeV2.waitingFor
+     */
+    fun waitingFor(serviceName: String, waitStrategy: WaitStrategy) = apply {
+        when (this.version) {
+            DockerComposeVersion.V1 -> v1.waitingFor(serviceName, waitStrategy)
+            DockerComposeVersion.V2 -> v2.waitingFor(serviceName, waitStrategy)
+        }
+    }
+
+    /**
+     * Returns [ContainerState] for the service with the given name.
+     * @see DockerComposeV1.getContainerByServiceName
+     * @see DockerComposeV2.getContainerByServiceName
+     */
+    fun getContainerByServiceName(serviceName: String): Optional<ContainerState> {
+        return when (this.version) {
+            DockerComposeVersion.V1 -> v1.getContainerByServiceName(serviceName)
+            DockerComposeVersion.V2 -> v2.getContainerByServiceName(serviceName)
+        }
+    }
+
+    /**
+     * Starts the Docker-Compose container.
+     * @see DockerComposeV1.start
+     * @see DockerComposeV2.start
+     */
+    fun start() {
+        when (this.version) {
+            DockerComposeVersion.V1 -> v1.start()
+            DockerComposeVersion.V2 -> v2.start()
+        }
+    }
+
+    private fun DockerComposeRemoveImagesStrategy.toV1(): DockerComposeV1.RemoveImages? {
+        return when (this) {
+            DockerComposeRemoveImagesStrategy.NONE -> null
+            DockerComposeRemoveImagesStrategy.ALL -> DockerComposeV1.RemoveImages.ALL
+            DockerComposeRemoveImagesStrategy.LOCAL -> DockerComposeV1.RemoveImages.LOCAL
+        }
+    }
+
+    private fun DockerComposeRemoveImagesStrategy.toV2(): DockerComposeV2.RemoveImages? {
+        return when (this) {
+            DockerComposeRemoveImagesStrategy.NONE -> null
+            DockerComposeRemoveImagesStrategy.ALL -> DockerComposeV2.RemoveImages.ALL
+            DockerComposeRemoveImagesStrategy.LOCAL -> DockerComposeV2.RemoveImages.LOCAL
+        }
+    }
+}
---
+++ src/test/kotlin/org/jholsten/me2e/container/docker/DockerComposeTest.kt
@@ -0,0 +1,116 @@
+package org.jholsten.me2e.container.docker
+
+import io.mockk.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.testcontainers.containers.wait.strategy.WaitAllStrategy
+import org.testcontainers.containers.wait.strategy.WaitStrategy
+import java.io.File
+import kotlin.test.*
+import org.testcontainers.containers.ComposeContainer as DockerComposeV2
+import org.testcontainers.containers.DockerComposeContainer as DockerComposeV1
+
+internal class DockerComposeTest {
+
+    private val dockerComposeFile: File = org.jholsten.me2e.parsing.utils.FileUtils.getResourceAsFile("docker-compose-parsing-test.yml")
+    private val waitStrategy: WaitStrategy = mockk<WaitAllStrategy>()
+
+    @BeforeTest
+    fun beforeTest() {
+        mockkConstructor(DockerCompose.KDockerComposeV1::class)
+        mockkConstructor(DockerComposeV2::class)
+        every { anyConstructed<DockerCompose.KDockerComposeV1>().start() } just runs
+        every { anyConstructed<DockerComposeV2>().start() } just runs
+    }
+
+    @AfterTest
+    fun afterTest() {
+        unmockkAll()
+    }
+
+    @Test
+    fun `Using Docker-Compose V1 should use V1 implementation`() {
+        val compose = DockerCompose("identifier", dockerComposeFile, version = DockerComposeVersion.V1)
+
+        compose.withLocalCompose(true)
+            .withBuild(true)
+            .withRemoveImages(DockerComposeRemoveImagesStrategy.ALL)
+            .withRemoveVolumes(true)
+        compose.waitingFor("service", waitStrategy)
+        compose.getContainerByServiceName("service")
+        compose.start()
+
+        verifyAll(shouldHaveUsedV1 = true, shouldHaveUsedV2 = false)
+    }
+
+    @Test
+    fun `Using Docker-Compose V2 should use V2 implementation`() {
+        val compose = DockerCompose("identifier", dockerComposeFile, version = DockerComposeVersion.V2)
+
+        compose.withLocalCompose(true)
+            .withBuild(true)
+            .withRemoveImages(DockerComposeRemoveImagesStrategy.ALL)
+            .withRemoveVolumes(true)
+        compose.waitingFor("service", waitStrategy)
+        compose.getContainerByServiceName("service")
+        compose.start()
+
+        verifyAll(shouldHaveUsedV1 = false, shouldHaveUsedV2 = true)
+    }
+
+    @ParameterizedTest(name = "[{index}] {0} should be mapped to {1}")
+    @CsvSource(
+        "NONE, null",
+        "ALL, ALL",
+        "LOCAL, LOCAL",
+        nullValues = ["null"]
+    )
+    fun `Docker compose remove images strategy should be mapped to V1 enum`(
+        strategy: DockerComposeRemoveImagesStrategy,
+        v1Enum: DockerComposeV1.RemoveImages?
+    ) {
+        val compose = DockerCompose("identifier", dockerComposeFile, version = DockerComposeVersion.V1)
+
+        compose.withRemoveImages(strategy)
+
+        verify { anyConstructed<DockerCompose.KDockerComposeV1>().withRemoveImages(v1Enum) }
+    }
+
+    @ParameterizedTest(name = "[{index}] {0} should be mapped to {1}")
+    @CsvSource(
+        "NONE, null",
+        "ALL, ALL",
+        "LOCAL, LOCAL",
+        nullValues = ["null"]
+    )
+    fun `Docker compose remove images strategy should be mapped to V2 enum`(
+        strategy: DockerComposeRemoveImagesStrategy,
+        v2Enum: DockerComposeV2.RemoveImages?
+    ) {
+        val compose = DockerCompose("identifier", dockerComposeFile, version = DockerComposeVersion.V2)
+
+        compose.withRemoveImages(strategy)
+
+        verify { anyConstructed<DockerComposeV2>().withRemoveImages(v2Enum) }
+    }
+
+    private fun verifyAll(shouldHaveUsedV1: Boolean, shouldHaveUsedV2: Boolean) {
+        val expectedV1 = if (shouldHaveUsedV1) 1 else 0
+        verify(exactly = expectedV1) { anyConstructed<DockerCompose.KDockerComposeV1>().withLocalCompose(true) }
+        verify(exactly = expectedV1) { anyConstructed<DockerCompose.KDockerComposeV1>().withBuild(true) }
+        verify(exactly = expectedV1) { anyConstructed<DockerCompose.KDockerComposeV1>().withRemoveImages(DockerComposeV1.RemoveImages.ALL) }
+        verify(exactly = expectedV1) { anyConstructed<DockerCompose.KDockerComposeV1>().withRemoveVolumes(true) }
+        verify(exactly = expectedV1) { anyConstructed<DockerCompose.KDockerComposeV1>().waitingFor("service", waitStrategy) }
+        verify(exactly = expectedV1) { anyConstructed<DockerCompose.KDockerComposeV1>().getContainerByServiceName("service") }
+        verify(exactly = expectedV1) { anyConstructed<DockerCompose.KDockerComposeV1>().start() }
+
+        val expectedV2 = if (shouldHaveUsedV2) 1 else 0
+        verify(exactly = expectedV2) { anyConstructed<DockerComposeV2>().withLocalCompose(true) }
+        verify(exactly = expectedV2) { anyConstructed<DockerComposeV2>().withBuild(true) }
+        verify(exactly = expectedV2) { anyConstructed<DockerComposeV2>().withRemoveImages(DockerComposeV2.RemoveImages.ALL) }
+        verify(exactly = expectedV2) { anyConstructed<DockerComposeV2>().withRemoveVolumes(true) }
+        verify(exactly = expectedV2) { anyConstructed<DockerComposeV2>().waitingFor("service", waitStrategy) }
+        verify(exactly = expectedV2) { anyConstructed<DockerComposeV2>().getContainerByServiceName("service") }
+        verify(exactly = expectedV2) { anyConstructed<DockerComposeV2>().start() }
+    }
+}
