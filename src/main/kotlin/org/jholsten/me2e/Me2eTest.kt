75e2b3b8af530f6b25f6a06cbca91899db407a3d
Joana <jholsten@uni-bremen.de>
2024-01-10 12:43:48 +0100

    adds base class for defining tests

---
+++ src/main/kotlin/org/jholsten/me2e/Me2eTest.kt
@@ -0,0 +1,47 @@
+package org.jholsten.me2e
+
+import org.jholsten.me2e.config.model.TestConfig
+import org.jholsten.me2e.container.ContainerManager
+import org.jholsten.me2e.container.injection.InjectionUtils
+import org.jholsten.me2e.mock.MockServerManager
+import org.jholsten.me2e.parsing.utils.FileUtils
+
+/**
+ * Base class for the definition of ME2E-Tests.
+ */
+open class Me2eTest {
+    companion object {
+        /**
+         * Configuration annotation that is used to configure the tests.
+         */
+        val configAnnotation = Me2eTestConfigScanner.findFirstTestConfigAnnotation()
+
+        /**
+         * Parsed test configuration.
+         */
+        val config: TestConfig = configAnnotation.format.parser.parseFile(configAnnotation.config)
+
+        /**
+         * Container manager instance responsible for managing the containers.
+         */
+        val containerManager = ContainerManager(
+            dockerComposeFile = FileUtils.getResourceAsFile(config.environment.dockerCompose),
+            dockerConfig = config.docker,
+            containers = config.environment.containers,
+        )
+
+        /**
+         * Mock server manager instance responsible for managing the mock servers.
+         */
+        val mockServerManager = MockServerManager(mockServers = config.environment.mockServers)
+
+        init {
+            mockServerManager.start()
+            containerManager.start()
+        }
+    }
+
+    init {
+        InjectionUtils(this).injectContainers()
+    }
+}
---
+++ src/test/kotlin/org/jholsten/me2e/Me2eTestIT.kt
@@ -0,0 +1,44 @@
+package org.jholsten.me2e
+
+import org.jholsten.me2e.config.model.ConfigFormat
+import org.jholsten.me2e.container.Container
+import org.jholsten.me2e.container.injection.InjectContainer
+import org.jholsten.me2e.container.microservice.MicroserviceContainer
+import kotlin.test.*
+
+@Me2eTestConfig(
+    config = "me2e-config-test.yaml",
+    format = ConfigFormat.YAML,
+)
+class Me2eTestIT : Me2eTest() {
+
+    @InjectContainer
+    private lateinit var backendApi: MicroserviceContainer
+
+    @InjectContainer("backend-api")
+    private lateinit var backendApiAlt: MicroserviceContainer
+
+    @InjectContainer
+    private lateinit var database: Container
+
+    // TODO: Inject Mockserver
+
+    @Test
+    fun `Initializing test class should start environment and inject containers`() {
+        assertEquals("me2e-config-test.yaml", configAnnotation.config)
+        assertEquals(ConfigFormat.YAML, configAnnotation.format)
+        for (container in containerManager.containers.values) {
+            assertTrue(container.isRunning)
+        }
+        assertTrue(mockServerManager.isRunning)
+        for (mockServer in mockServerManager.mockServers.values) {
+            assertTrue(mockServer.isRunning)
+        }
+        assertNotNull(backendApi)
+        assertSame(containerManager.containers["backend-api"], backendApi)
+        assertNotNull(backendApiAlt)
+        assertSame(containerManager.containers["backend-api"], backendApiAlt)
+        assertNotNull(database)
+        assertSame(containerManager.containers["database"], database)
+    }
+}
