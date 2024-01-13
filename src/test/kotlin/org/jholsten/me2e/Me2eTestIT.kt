bc55b6272edfbc869963d82fac6b437d385ade01
Joana <jholsten@uni-bremen.de>
2024-01-13 18:04:56 +0100

    adds test for invoking mock server in container

--- src/test/kotlin/org/jholsten/me2e/Me2eTestIT.kt
+++ src/test/kotlin/org/jholsten/me2e/Me2eTestIT.kt
@@ -1,9 +1,14 @@
 package org.jholsten.me2e
 
+import org.jholsten.me2e.assertions.Assertions.Companion.assertThat
+import org.jholsten.me2e.assertions.Assertions.Companion.isEqualTo
 import org.jholsten.me2e.config.model.ConfigFormat
 import org.jholsten.me2e.container.Container
 import org.jholsten.me2e.container.injection.InjectContainer
 import org.jholsten.me2e.container.microservice.MicroserviceContainer
+import org.jholsten.me2e.request.model.HttpRequestBody
+import org.jholsten.me2e.request.model.MediaType
+import org.jholsten.me2e.request.model.RelativeUrl
 import org.junit.jupiter.api.AfterAll
 import kotlin.test.*
 
@@ -51,5 +56,18 @@ class Me2eTestIT : Me2eTest() {
         assertSame(containerManager.containers["backend-api"], backendApiAlt)
         assertNotNull(database)
         assertSame(containerManager.containers["database"], database)
+    }
+
+    @Test
+    fun `Invoking mock server in container should succeed`() {
+        val response = backendApi.post(RelativeUrl("/search"), HttpRequestBody(content = "{\"id\": 123}", MediaType.JSON_UTF8))
+
+        assertThat(response).statusCode(isEqualTo(200))
+        assertThat(response).jsonBody("id", isEqualTo("123"))
+        assertThat(response).jsonBody("items[0].name", isEqualTo("A"))
+        assertThat(response).jsonBody("items[0].value", isEqualTo("42"))
+        assertThat(response).jsonBody("items[1].name", isEqualTo("B"))
+        assertThat(response).jsonBody("items[1].value", isEqualTo("1"))
+        // TODO: Verify mock server
     }
 }
