30b6594ab89fb5d03e05ab150aee838f464c2e41
Joana <jholsten@uni-bremen.de>
2024-01-20 09:31:45 +0100

    adds support for serializing timestamps

--- src/main/kotlin/org/jholsten/me2e/parsing/utils/DeserializerFactory.kt
+++ src/main/kotlin/org/jholsten/me2e/parsing/utils/DeserializerFactory.kt
@@ -2,7 +2,9 @@ package org.jholsten.me2e.parsing.utils
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 
 internal class DeserializerFactory private constructor() {
@@ -10,12 +12,16 @@ internal class DeserializerFactory private constructor() {
         @JvmStatic
         private val YAML_MAPPER: ObjectMapper = YAMLMapper()
             .registerModule(KotlinModule.Builder().build())
+            .registerModule(JavaTimeModule())
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
 
         @JvmStatic
         private val OBJECT_MAPPER: ObjectMapper = ObjectMapper()
             .registerModule(KotlinModule.Builder().build())
+            .registerModule(JavaTimeModule())
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
 
         @JvmStatic
         fun getYamlMapper(): ObjectMapper {
